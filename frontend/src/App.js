83bc97250bcefc0c1ec151f5c14b42a90efa472b
Utkarsh Jaulkar <ujaulkar1604@gmail.com>
2023-02-17 15:34:15 +0530

    Added goal functionality

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Header from './components/Header'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:5000'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
+  )
+}
+
+export default App
